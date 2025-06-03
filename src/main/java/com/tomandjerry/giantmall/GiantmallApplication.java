e25415980045c92e6d8e1820c212c0f1feed6610
ongsttt52 <ongsttt52@gmail.com>
2025-06-03 16:13:35 +0900

    init : 스프링부트 프로젝트 초기 설정

---
+++ src/main/java/com/tomandjerry/giantmall/GiantmallApplication.java
@@ -0,0 +1,13 @@
+package com.tomandjerry.giantmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiantmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiantmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomandjerry/giantmall/GiantmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomandjerry.giantmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiantmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
