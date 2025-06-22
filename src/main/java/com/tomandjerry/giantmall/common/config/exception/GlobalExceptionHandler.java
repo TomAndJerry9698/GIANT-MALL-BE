7265258f8544ae92fded93ba9d0c9b324fc449b0
song eojin <feshed528@gmail.com>
2025-06-22 11:06:18 +0900

    feat : 글로벌 예외 처리 및 커스텀 에러 코드 시스템 구현
    - ErrorCode enum을 통해 비즈니스 에러 코드, 메시지, HTTP 상태 정의
    - CustomException 클래스에서 ErrorCode 기반 예외 생성 및 상태 반환 기능 추가
    - GlobalExceptionHandler에서 CustomException, ValidationException, 일반 Exception 핸들링 구현
    - ApiResponseDto를 사용하여 일관된 에러 응답 반환

---
+++ src/main/java/com/tomandjerry/giantmall/common/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.tomandjerry.giantmall.common.config.exception;
+
+import com.tomandjerry.giantmall.common.config.dto.ApiResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<ApiResponseDto<?>> handleCustomException(CustomException e) {
+        return ResponseEntity
+                .status(e.getStatus())
+                .body(ApiResponseDto.fail(e.getMessage()));
+    }
+
+    @ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponseDto<?>> handleValidationException(org.springframework.web.bind.MethodArgumentNotValidException e) {
+        String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+        return ResponseEntity
+                .badRequest()
+                .body(ApiResponseDto.fail(errorMessage));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponseDto<?>> handleException(Exception e) {
+        return ResponseEntity
+                .internalServerError()
+                .body(ApiResponseDto.fail("서버 오류가 발생했습니다."));
+    }
+}
