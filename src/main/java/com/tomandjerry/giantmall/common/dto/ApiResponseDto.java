cde3bf4ca4085746bc248a183905eb3b42575108
song eojin <feshed528@gmail.com>
2025-06-22 13:24:56 +0900

    refactor : 빌더 제거 및 정적 팩토리 메서드 내 new 방식으로 개선 (ApiResponseDto)

--- src/main/java/com/tomandjerry/giantmall/common/dto/ApiResponseDto.java
+++ src/main/java/com/tomandjerry/giantmall/common/dto/ApiResponseDto.java
@@ -1,14 +1,10 @@
 package com.tomandjerry.giantmall.common.dto;
 
-import lombok.Builder;
-
-@Builder
 public record ApiResponseDto<T>(boolean success, String message, T data) {
     public static <T> ApiResponseDto<T> success(T data, String message) {
-        return ApiResponseDto.<T>builder().success(true).message(message).data(data).build();
+        return new ApiResponseDto<>(true, message, data);
     }
-
     public static <T> ApiResponseDto<T> fail(String message) {
-        return ApiResponseDto.<T>builder().success(false).message(message).data(null).build();
+        return new ApiResponseDto<>(false, message, null);
     }
 }
