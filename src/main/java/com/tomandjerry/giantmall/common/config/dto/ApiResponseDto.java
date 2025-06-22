7806771ec5a876a0bba9f65ddc6f90b93088c4f8
song eojin <feshed528@gmail.com>
2025-06-22 11:07:07 +0900

    feat : 공통 API 응답 DTO 구현 (ApiResponseDto)

---
+++ src/main/java/com/tomandjerry/giantmall/common/config/dto/ApiResponseDto.java
@@ -0,0 +1,14 @@
+package com.tomandjerry.giantmall.common.config.dto;
+
+import lombok.Builder;
+
+@Builder
+public record ApiResponseDto<T>(boolean success, String message, T data) {
+    public static <T> ApiResponseDto<T> success(T data, String message) {
+        return ApiResponseDto.<T>builder().success(true).message(message).data(data).build();
+    }
+
+    public static <T> ApiResponseDto<T> fail(String message) {
+        return ApiResponseDto.<T>builder().success(false).message(message).data(null).build();
+    }
+}
