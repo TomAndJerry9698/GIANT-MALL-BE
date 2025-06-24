5b780fc1798314aa8c46ed63d5e5015858ab5850
song eojin <feshed528@gmail.com>
2025-06-24 23:58:34 +0900

    refactor : 응답 실패는 Exception와 GlobalExceptionHandler에서 처리하도록 fail 메서드 제거 (ApiResponseDto)

--- src/main/java/com/tomandjerry/giantmall/common/dto/ApiResponseDto.java
+++ src/main/java/com/tomandjerry/giantmall/common/dto/ApiResponseDto.java
@@ -1,10 +1,8 @@
 package com.tomandjerry.giantmall.common.dto;
 
 public record ApiResponseDto<T>(boolean success, String message, T data) {
+
     public static <T> ApiResponseDto<T> success(T data, String message) {
         return new ApiResponseDto<>(true, message, data);
-    }
-    public static <T> ApiResponseDto<T> fail(String message) {
-        return new ApiResponseDto<>(false, message, null);
     }
 }
